e42483ed7ea5428db118c5cf07977189ecea8e5d
Adebayo Temitope <adecancode@gmail.com>
2022-04-15 19:02:44 +0100

    changes

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signUp(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+      delete user.password;
+
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('User already exists');
+        }
+      }
+      throw err;
+    }
+  }
+
+  async signIn(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('User does not exist');
+    }
+
+    const isValid = argon.verify(user.password, dto.password);
+
+    if (!isValid) {
+      throw new ForbiddenException('Password Incorrect');
+    }
+
+    delete user.password;
+    return user;
+  }
+}
